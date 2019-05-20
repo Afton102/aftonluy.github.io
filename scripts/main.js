1c818f8f16f27d93b247f6a201bca9548c7a1c6d
Afton102 <50365870+Afton102@users.noreply.github.com>
2019-05-20 22:03:56 +0500

    файлы для первого сайта

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var iceCream = 'chocolate';
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+if (iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('Awwww, but chocolate is my favorite...');
+}
+function multiply(num1,num2) {
+  var result = num1 * num2;
+  return result;
+}
